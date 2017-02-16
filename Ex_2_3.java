809146f4e5a9e35be2d4e2fcca9c4fcfd32d91b4
AlexRubenko <alexrubenko80@gmail.com>
2017-02-16 23:29:50 +0200

    Add files via upload
    Exercise 2 (3)

---
+++ Ex_2_3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Ex_2_3
+{
+    public static void main(String[]args)
+    {
+        int tens = 0;
+        int units = 0;
+
+        System.out.println("Enter a two-digit number:");
+
+        int a = Integer.parseInt(args[0]);
+
+        if (a<100)
+        {
+            tens = a / 10;
+            units = a%10;
+        }
+        else System.out.println("You have entered not a two-digit number!" );
+
+        System.out.println("Your number before swap operation : " + a);
+        System.out.println("Sum of numbers: "  + (10*units + tens));
+    }
+}
