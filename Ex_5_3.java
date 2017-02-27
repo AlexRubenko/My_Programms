29797a3afc319116a9b08754615e50f8e891cf57
Alex Rubenko <alexrubenko80@gmail.com>
2017-02-27 19:48:07 +0200

    Add files via upload

---
+++ Ex_5_3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex_5_3
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter integer number > 1.");
+
+        int a = sc.nextInt();
+
+        sc.close();
+
+        ifPrimeNumber(a);
+    }
+    public static void ifPrimeNumber(int b)
+    {
+        if(b>1 && b%b==0 && b%2!=0)
+        {
+            System.out.println("Yes, " + b + " is prime number!");
+        }
+        else System.out.println( b + " isn't prime number!");
+    }
+}
